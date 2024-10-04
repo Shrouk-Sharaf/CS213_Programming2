61f25b18122b12ee3b57bef199e3e39e173e93ea
Shrouk-Sharaf <sayedahmedroka@gmail.com>
2024-10-04 23:06:42 +0300

    I edited problem 3

--- problem 1 _ T1 _ A1.cpp
+++ problem 1 _ T1 _ A1.cpp
@@ -2,20 +2,31 @@
 #include <vector>
 #include <regex>
 using namespace std;
+
 int main() {
-    string sentence;
+    string sentence, line;
     vector<pair<string, string>> replacements = {
             {"he", "he or she"},
             {"He", "He or she"},
             {"him", "him or her"},
             {"his", "his or her"},
-            {"His", "His or her"}
+            {"His", "His or her"},
+            {"himself", "himself or herself"}
     };
-    cout << "Enter your sentence\n";
-    getline(cin, sentence);
+
+    cout << "Enter your sentence (press Enter on an empty line to finish):\n";
+
+    while (true) {
+        getline(cin, line);
+        if (line.empty()) break;
+        sentence += line + "\n";
+    }
+
     for (const auto& replacement : replacements) {
         regex word("\\b" + replacement.first + "\\b");
         sentence = regex_replace(sentence, word, replacement.second);
     }
+
+    // Output the final result
     cout << sentence << endl;
 }
